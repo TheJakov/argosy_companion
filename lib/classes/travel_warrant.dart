72cff9d9037cba62ad1d7239b3a45240b87a47d4
Luka Rokolj <luka.rokolj.2020@uni.strath.ac.uk>
2022-06-19 14:58:47 +0200

    Travel warrant detail screen

--- lib/classes/travel_warrant.dart
+++ lib/classes/travel_warrant.dart
@@ -5,29 +5,40 @@ class TravelWarrant {
   String destination;
   String reason;
   String remark;
+  String meansOfTransportation;
+  String advances;
   bool isNew;
 
-  TravelWarrant(
-      this.person,
-      this.dateFrom,
-      this.dateTo,
-      this.destination,
-      this.reason,
-      this.remark,
-      {this.isNew = false}
-      );
+  TravelWarrant(this.person, this.dateFrom, this.dateTo, this.destination,
+      this.reason, this.remark, this.meansOfTransportation, this.advances,
+      {this.isNew = false});
 
-  String datePeriod(){
+  String datePeriod() {
     return '$dateFrom - $dateTo';
   }
 
-  static List<TravelWarrant> fetchDataTest(){
+  static List<TravelWarrant> fetchDataTest() {
     return <TravelWarrant>[
-      TravelWarrant('Jakov Kristović', '05.06.2022.', '07.06.2022.', 'Zagreb', '.debug konferencija', 'Putovanje avionom'),
-      TravelWarrant('Luka Rokolj', '15.06.2022.', '20.08.2022.', 'Glasgow', 'Sveučilišna posla', 'Ide preko volje'),
-      TravelWarrant('Jack Sparrow', '02.07.2022.', '13.07.2022.', 'Black Pearl', 'Promoviranje ruma', 'Ima li ga još?', isNew: true),
-      TravelWarrant('Davy Jones', '05.07.2022.', '10.07.2022.', 'The Flying Dutchman', 'Đir oko Kariba', '', isNew: true),
-      TravelWarrant('Hector Barbosa', '23.07.2022.', '24.07.2022.', 'Isle de Muerte', 'Da guzica vidi puta', '', isNew: true),
+      TravelWarrant('Jakov Kristović', '05.06.2022.', '07.06.2022.', 'Zagreb',
+          '.debug konferencija', 'Putovanje avionom', 'Avion', '/'),
+      TravelWarrant('Luka Rokolj', '15.06.2022.', '20.08.2022.', 'Glasgow',
+          'Sveučilišna posla', 'Ide preko volje', 'Avion', '/'),
+      TravelWarrant('Jack Sparrow', '02.07.2022.', '13.07.2022.', 'Black Pearl',
+          'Promoviranje ruma', 'Ima li ga još?', 'Dingy', '350kn',
+          isNew: true),
+      TravelWarrant(
+          'Davy Jones',
+          '05.07.2022.',
+          '10.07.2022.',
+          'The Flying Dutchman',
+          'Đir oko Kariba',
+          '',
+          'The Flying Dutchman',
+          '/',
+          isNew: true),
+      TravelWarrant('Hector Barbosa', '23.07.2022.', '24.07.2022.',
+          'Isle de Muerte', 'Da guzica vidi puta', '', 'Black Pearl', '/',
+          isNew: true),
     ];
   }
 }
--- lib/screens/travel_warrant_detail.dart
+++ lib/screens/travel_warrant_detail.dart
@@ -1,12 +1,12 @@
+import 'package:argosy_companion/classes/argosy_companion_constants.dart';
 import 'package:argosy_companion/classes/travel_warrant.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TravelWarrantDetail extends StatefulWidget {
   final TravelWarrant travelWarrant;
 
-  const TravelWarrantDetail(
-      this.travelWarrant,
-      {Key? key}) : super(key: key);
+  const TravelWarrantDetail(this.travelWarrant, {Key? key}) : super(key: key);
 
   @override
   State<TravelWarrantDetail> createState() => _TravelWarrantDetailState();
@@ -16,13 +16,13 @@ class _TravelWarrantDetailState extends State<TravelWarrantDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 5),
-        child:  Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              color: Color(ArgosyCompanionConstants.travelWarrantHex),
+              child: Row(
                 children: [
                   TextButton(
                       child: const Icon(
@@ -43,39 +43,262 @@ class _TravelWarrantDetailState extends State<TravelWarrantDetail> {
                   ),
                 ],
               ),
-              Container(
+            ),
+            Container(
                 height: MediaQuery.of(context).size.height * 0.75,
-                child:
-                    SingleChildScrollView(
-                      child:
-                          Column (
-                            children: [
-                              Row(
-                                children: [
-                                Container(
-                                  width: MediaQuery.of(context).size.width,
-                                  decoration: const BoxDecoration(color: Colors.redAccent),
-                                  child: Text('${widget.travelWarrant.person}\nOvako se pristupa predanom prop u state\nOvdje je list view'
-                                      + '\na\nb\nc\nd\ne\nf\ng\nh\na\nb\nc\nd\ne\nf\ng\nh\na\nb\nc\nd\ne\nf\ng\nh'
-                                      + '\na\nb\nc\nd\ne\nf\ng\nh\na\nb\nc\nd\ne\nf\ng\nh\na\nb\nc\nd\ne\nf\ng\nh'),
-                                ),
-                              ])
-                            ],
-                          )
-                    )
-              ),
-              Row(
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.15,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: const BoxDecoration(color: Colors.lightBlueAccent),
-                    child: const Text('Ovdje ide dio s odobravanjem'),
+                child: SingleChildScrollView(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Row(children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text("Radnik",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.black)),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text('${widget.travelWarrant.person}',
+                                style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black)),
+                          ],
+                        ),
+                      ]),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text("Datum putovanja",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.black)),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text('${widget.travelWarrant.datePeriod()}',
+                                style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black)),
+                          ],
+                        ),
+                      ]),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text("Odredište",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.black)),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text('${widget.travelWarrant.destination}',
+                                style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black)),
+                          ],
+                        ),
+                      ]),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text("Razlog",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.black)),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text('${widget.travelWarrant.reason}',
+                                style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black)),
+                          ],
+                        ),
+                      ]),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text("Napomena",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.black)),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text('${widget.travelWarrant.remark}',
+                                style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black)),
+                          ],
+                        ),
+                      ]),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text("Prijevozna sredstva",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.black)),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                                '${widget.travelWarrant.meansOfTransportation}',
+                                style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black)),
+                          ],
+                        ),
+                      ]),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text("Akontacije",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.black)),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text('${widget.travelWarrant.advances}',
+                                style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black)),
+                          ],
+                        ),
+                      ]),
+                    ],
                   ),
-                ],
-              ),
-            ],
-          ),
+                ))),
+            Row(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.15,
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.07,
+                        width: MediaQuery.of(context).size.width,
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(
+                            Icons.edit_calendar,
+                            size: 28,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      Container(
+                          height: MediaQuery.of(context).size.height * 0.08,
+                          width: MediaQuery.of(context).size.width,
+                          color: Colors.greenAccent,
+                          child: Row(
+                            children: [
+                              TextButton(
+                                  style: TextButton.styleFrom(
+                                      padding: EdgeInsets.zero,
+                                      minimumSize: Size(50, 30),
+                                      tapTargetSize:
+                                          MaterialTapTargetSize.shrinkWrap,
+                                      alignment: Alignment.centerLeft),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Container(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.08,
+                                    width:
+                                        MediaQuery.of(context).size.width / 2,
+                                    alignment: Alignment.center,
+                                    color: Colors.lightBlueAccent,
+                                    child: Text(
+                                      "ODOBRI",
+                                      style: GoogleFonts.quicksand(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  )),
+                              TextButton(
+                                  style: TextButton.styleFrom(
+                                      padding: EdgeInsets.zero,
+                                      minimumSize: Size(50, 30),
+                                      tapTargetSize:
+                                          MaterialTapTargetSize.shrinkWrap,
+                                      alignment: Alignment.centerLeft),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Container(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.08,
+                                    width:
+                                        MediaQuery.of(context).size.width / 2,
+                                    alignment: Alignment.center,
+                                    color: Colors.redAccent,
+                                    child: Text(
+                                      "ODBIJ",
+                                      style: GoogleFonts.quicksand(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  )),
+                            ],
+                          )),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
