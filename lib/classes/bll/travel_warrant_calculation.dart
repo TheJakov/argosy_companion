c7aeb1966946c6d2a84922e9d1115fc4208bae4e
TheJakov <kristovic.jakov@gmail.com>
2022-07-23 12:48:03 +0200

    wage + TravelWarrantCalculation extended props

--- lib/classes/bll/travel_warrant_calculation.dart
+++ lib/classes/bll/travel_warrant_calculation.dart
@@ -1,16 +1,23 @@
 import 'package:argosy_companion/classes/bll/travel_warrant.dart';
+import 'package:argosy_companion/classes/bll/wage.dart';
 
 class TravelWarrantCalculation extends TravelWarrant {
 
+  List<Wage> wages;
+  List<String> transportationExpenses;
+  List<String> otherExpenses;
+
   TravelWarrantCalculation(id, person, dateFrom, dateTo, destination, reason, remark,
-      meansOfTransportation, advances, {isNew = false, projects = const []} ) :
+      meansOfTransportation, advances, {isNew = false, projects = const [],
+        this.wages = const [], this.transportationExpenses = const [], this.otherExpenses = const []}) :
       super (id, person, dateFrom, dateTo, destination, reason, remark,
           meansOfTransportation, advances);
 
   static List<TravelWarrantCalculation> fetchDataTest() {
     return <TravelWarrantCalculation>[
       TravelWarrantCalculation(1, 'Jakov Kristović', '05.01.2022.', '09.01.2022.', 'New York',
-          'Poslovna konferencija', '.NET Days' , 'Avion', '', projects: <String>["PROJ11", "PROJ12"]),
+          'Poslovna konferencija', '.NET Days' , 'Avion', '',
+          projects: <String>["PROJ11", "PROJ12"], wages: <Wage>[Wage('SAD', 4.00, 'HRK', 1600.00)]),
       TravelWarrantCalculation(2, 'Luka Rokolj', '12.02.2022.', '20.02.2022.', 'Amsterdam',
           'Službeni put', 'Funny Cake Days', 'Vlak', '1100 €'),
       TravelWarrantCalculation(3, 'Jack Sparrow', '13.02.2022.', '15.02.2022.', 'Port Royale',
---
+++ lib/classes/bll/wage.dart
@@ -0,0 +1,8 @@
+class Wage {
+  String country;
+  double numberOfDays;
+  String currency;
+  double total;
+
+  Wage(this.country, this.numberOfDays, this.currency, this.total);
+}
